36450beb98d9f98aa5fc06a590f6f183ddb4b0b6
gabriel-demes <Gabrieldemes@nyu.edu>
2021-04-06 20:17:10 -0400

    changed api key

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  post 'entries/:id/sentiment', to: 'entries#sentiment'
+  patch 'entries/:id/sentiment', to: 'entries#sentiment'
 end
